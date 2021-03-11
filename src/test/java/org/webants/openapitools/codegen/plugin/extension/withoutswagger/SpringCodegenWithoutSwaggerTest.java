0c136e192bc251e490dd58fef513bd899f2ddef3
Sebastian Steinfeld <Sebastian.Steinfeld@rewe-group.com>
2021-03-11 09:30:05 +0100

    extend configuration for testing

--- src/test/java/org/webants/openapitools/codegen/plugin/extension/withoutswagger/SpringCodegenWithoutSwaggerTest.java
+++ src/test/java/org/webants/openapitools/codegen/plugin/extension/withoutswagger/SpringCodegenWithoutSwaggerTest.java
@@ -31,13 +31,18 @@ public class SpringCodegenWithoutSwaggerTest {
 
         Map<String, Object> additionalProperties = new HashMap<>();
         additionalProperties.put(CodegenConstants.SOURCE_FOLDER, "."); // skip supporting files => to avoid globalProperties - supportingFiles
+        additionalProperties.put(SpringCodegenWithoutSwagger.DELEGATE_PATTERN, Boolean.TRUE.toString());
+        additionalProperties.put(SpringCodegenWithoutSwagger.API_FIRST, Boolean.FALSE.toString());
 
 
         CodegenConfigurator configurator = new CodegenConfigurator()
                 .setGeneratorName(SpringCodegenWithoutSwagger.GENERATOR_NAME)
                 .setInputSpec("v3_0_petstore.json")
-                .setOutputDir("target/junit-test4withoutSwagger")
+                .setOutputDir("target/generated-test-sources/test4withoutSwagger")
+                .setOutputDir("build/test4withoutSwagger")
                 .setLibrary(SpringCodegenWithoutSwagger.LIBRARY_WITHOUT_SWAGGER)
+                .setModelPackage("com.petstore.entity")
+                .setApiPackage("com.petstore.boundary")
 //                .setGlobalProperties(globalProperties)
                 .setAdditionalProperties(additionalProperties)
                 ;
